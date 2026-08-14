5f2044dc04e4792ff144cf0a1f2a75e912219e60
Konstantin S. <havendv@gmail.com>
2026-08-14 19:46:17 +0400

    feat: Updated OpenAPI spec (#31)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Reve/Generated/Reve.ReveClient.g.cs
+++ src/libs/Reve/Generated/Reve.ReveClient.g.cs
@@ -34,10 +34,17 @@ namespace Reve
 
         /// <inheritdoc/>
         public global::Reve.AutoSDKClientOptions Options { get; }
+
+        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::Reve.SourceGenerationContext.Default);
+
         /// <summary>
         /// 
         /// </summary>
-        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext { get; set; } = global::Reve.SourceGenerationContext.Default;
+        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext
+        {
+            get => JsonSerializerContextProvider.Value;
+            set => JsonSerializerContextProvider = new(() => value);
+        }
 
 
         /// <summary>
